29192e27db171dd9297b9ef86daa0d6bc5efc1ce
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-31 10:52:02 -0300

    Estrutura da aplicação - Cores & fonte

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -20,7 +20,7 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   border: 0;
   margin: 8px;
 
-  background-color: ${props => props.theme.primary};
+  background-color: ${props => props.theme['green-500']};
   color: ${props => props.theme.white};
 
   /* ${props => {
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,19 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+  }
+
   body {
-    background: #333;
-    color: #FFF;
+    background: ${props => props.theme['gray-900']};
+    color: ${props => props.theme['gray-300']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
   }
 `;
