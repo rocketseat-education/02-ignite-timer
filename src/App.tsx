928b4e791f492701ec14b639ba3a9b22fd8f5f9b
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-05-31 10:42:36 -0300

    Estrutura da aplicação - Estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from './components/Button';
 
+import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
@@ -11,6 +12,8 @@ export function App() {
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #333;
+    color: #FFF;
+  }
+`;
