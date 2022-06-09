d5bceb92ad7cd3a6935765086b8bc9376acd462e
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-06-09 15:28:04 -0300

    Páginas e rotas - Estilização do Layout

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/Header'
+
+import { LayoutContainer } from './styles'
+
+export function DefaultLayout() {
+  return (
+    <LayoutContainer>
+      <Header />
+      <Outlet />
+    </LayoutContainer>
+  )
+}
---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 74rem;
+  height: calc(100vh - 10rem);
+  margin: 5rem auto;
+  padding: 2.5rem;
+
+  background: ${(props) => props.theme['gray-800']};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+`
