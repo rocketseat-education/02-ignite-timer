700c0b8b5b4e65cd64bc1e1138a7349e90b9cf5f
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-06-30 17:19:37 -0300

    Contexto no React - Reset do formulário

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -69,8 +69,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -35,14 +35,19 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisable = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
