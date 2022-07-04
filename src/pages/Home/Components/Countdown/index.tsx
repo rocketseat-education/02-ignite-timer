280ad5e7452ab8dd68f0305f0010c93b83b8aaa7
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-07-04 15:41:46 -0300

    Reducers - Salvando estado no storage

--- src/pages/Home/Components/Countdown/index.tsx
+++ src/pages/Home/Components/Countdown/index.tsx
@@ -21,7 +21,7 @@ export function Countdown() {
       interval = setInterval(() => {
         const secondsDifference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDifference >= totalSeconds) {
