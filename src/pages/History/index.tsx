7af34f20db0c978c255359f3536b0663a2e2234c
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-07-07 18:40:58 -0300

    Reducers - Corrigindo listagem do histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -28,7 +28,7 @@ export function History() {
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
                       locale: ptBR,
                     })}
