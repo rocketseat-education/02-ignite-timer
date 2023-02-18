643216c412aa29a3fc5823cff07f3a85eb893094
natalia <lnataliaperri@gmail.com>
2023-02-18 11:37:41 -0300

    basic structure and component styles defined

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import { Router } from './Router'
 
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <CyclesContextProvider>
           <Router />
-        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,57 +1,5 @@
-import { useContext } from 'react'
-import { formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
-import { CyclesContext } from '../../contexts/CyclesContext'
-import { HistoryContainer, HistoryList, Status } from './styles'
-
-export function History() {
-  const { cycles } = useContext(CyclesContext)
-
+export function History () {
   return (
-    <HistoryContainer>
-      <h1>Meu histórico</h1>
-
-      <HistoryList>
-        <table>
-          <thead>
-            <tr>
-              <th>Tarefa</th>
-              <th>Duração</th>
-              <th>Duração</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cycles.map((cycle) => {
-              return (
-                <tr key={cycle.id}>
-                  <td>{cycle.task}</td>
-                  <td>{cycle.minutesAmount} minutos</td>
-                  <td>
-                    {formatDistanceToNow(new Date(cycle.startDate), {
-                      addSuffix: true,
-                      locale: ptBR,
-                    })}
-                  </td>
-                  <td>
-                    {cycle.finishedDate && (
-                      <Status statusColor="green">Concluído</Status>
-                    )}
-
-                    {cycle.interruptedDate && (
-                      <Status statusColor="red">Interrompido</Status>
-                    )}
-
-                    {!cycle.finishedDate && !cycle.interruptedDate && (
-                      <Status statusColor="yellow">Em andamento</Status>
-                    )}
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </HistoryList>
-    </HistoryContainer>
+      <div></div>
   )
 }
