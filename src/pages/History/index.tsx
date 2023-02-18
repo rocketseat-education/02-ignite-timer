83b56f671879187c183a73384b8c40448b0f5836
natalia <lnataliaperri@gmail.com>
2023-02-18 15:15:53 -0300

    status component styles done

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,51 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History () {
   return (
-      <div></div>
+      <HistoryContainer>
+        <h1>Meu histórico</h1>
+        <HistoryList>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Início</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                <Status statusColor="green">Concluído</Status>
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                <Status statusColor="yellow">Em andamento</Status>
+                </td>
+              </tr>
+            </tbody>
+            <tbody>
+              <tr>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há 2 meses</td>
+                <td>
+                <Status statusColor="red">Interrompido</Status>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </HistoryList>
+      </HistoryContainer>
   )
 }
