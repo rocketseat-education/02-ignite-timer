b968caf3530b220e34cba2de9997ed6f42346f9f
natalia <lnataliaperri@gmail.com>
2023-02-18 14:42:16 -0300

    home page structure and styles done

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,6 +16,7 @@ export const GlobalStyle = createGlobalStyle`
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
     -webkit-font-smoothing: antialiased;
+   
   }
 
   body, input, textarea, button {
